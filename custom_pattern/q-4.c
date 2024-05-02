3be464487602b4f47dc623fdbed13429be61acea
maleksahil <maleksahil9315@gmail.com>
2024-05-02 12:45:00 +0530

    pattern

---
+++ custom_pattern/q-4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+    int val=10;
+
+
+    for(int i=1; i<=val; i++){
+        for(int j=1; j<=val; j++){
+            if(i==1 || i==val || j==1 || j==val || i==j || i+j==val+1){
+                printf("* ");
+            }else{
+                printf("  ");
+            }
+            
+        }
+        printf("\n");
+    }
+}
